1238ec9da632a4b359274ed34a95dd517c498a3d
Ludmila2603 <mika22105@gmail.com>
2020-11-03 11:53:41 +0300

    deploy

---
+++ src/reduxStore/currency/actions.js
@@ -0,0 +1,49 @@
+import { currenciesApi } from '../../service/api/currency';
+import {
+	GET_CURRENCY_PAIRS,
+	ADD_CURRENCY_PAIRS,
+	DELETE_CURRENCY_PAIRS,
+	UPDATE_CURRENCY_PAIRS,
+	SAVE_OPTION_TIME_UPDATE,
+	SAVE_OPTION_VALUE_TO,
+	SAVE_OPTION_VALUE_FROM,
+} from './actionTypes';
+import fetch from "isomorphic-fetch";
+import {BASE_API_URL} from "../../config";
+
+export const getCurrencies = () => async (dispatch) => {
+	try {
+		const data = await currenciesApi.getApiCurrencies();
+		dispatch({ type: GET_CURRENCY_PAIRS, data }); // get currencies from api and dispatch in store
+	} catch (err) {
+		throw new Error(err);
+	}
+};
+
+export const updateCurrencyPair = (updatePair) => (dispatch) => {
+	// dispatch action with type to store -- updating selected pair at users list from serverAPI
+	dispatch({ type: UPDATE_CURRENCY_PAIRS, payload: updatePair });
+};
+
+export const addNewCurrencyPair = (currencyObject) => (dispatch) => {
+	// dispatch action with type to store -- adding  new pair in users list of currencies
+	dispatch({ type: ADD_CURRENCY_PAIRS, payload: currencyObject });
+};
+
+export const removeCurrencyPair = (id) => (dispatch) => {
+	// dispatch action with type to store -- removing selected pair at users list
+	dispatch({ type: DELETE_CURRENCY_PAIRS, payload: id });
+};
+
+export const saveValueFrom = (valueFrom) => (dispatch) => {
+	// dispatch action with type to store -- saving selected currency from to store
+	dispatch({ type: SAVE_OPTION_VALUE_FROM, payload: valueFrom });
+};
+export const saveValueTo = (valueTo) => (dispatch) => {
+	// dispatch action with type to store -- saving selected currency from to store
+	dispatch({ type: SAVE_OPTION_VALUE_TO, payload: valueTo });
+};
+export const saveValueTime = (time) => (dispatch) => {
+	// dispatch action with type to store -- saving selected timeupdate to store
+	dispatch({ type: SAVE_OPTION_TIME_UPDATE, payload: time });
+};
